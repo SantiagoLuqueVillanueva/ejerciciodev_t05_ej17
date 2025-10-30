be90d1679182fb9d652e7357e2a25649daaf1969
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-30 23:08:30 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num = Integer.parseInt(System.console().readLine("Introduzca un número entero positivo: "));
+
+        do {
+            if (num < 0) {
+                System.out.println("Debes introducir un número positivo");
+                System.out.println("Intentalo de nuevo.");
+                num = Integer.parseInt(System.console().readLine("Introduzca un número entero positivo: "));
+            }
+        } while (num < 0);
+        
+        int sol = 0;
+        
+        for (int i = num; i < num + 100; i++) {
+            sol += i;
+        }
+
+        System.out.printf("La suma de los 100 números siguientes a %d es %d.", num, sol);
     }
 }
